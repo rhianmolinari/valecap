ab1fc3b1d3afae64a8cec622d0cb963054b1f39a
Rhian Molinari <rhian.chat@gmail.com>
2013-01-10 22:59:22 -0200

    Correçoes no CSS

--- wp-content/themes/valecappneus/footer.php
+++ wp-content/themes/valecappneus/footer.php
@@ -12,6 +12,18 @@
 		<nav id="site-navigation" class="grid_9" role="navigation">
 			<?php wp_nav_menu( array( 'theme_location' => 'primary', 'menu_class' => 'nav-menu' ) ); ?>
 		</nav>
+		<div class="grid_3">
+			<ul>
+				<li>
+					<!-- HTML5 !-->
+					<a class="nolink" href="http://www.w3.org/html/logo/"><img src="http://www.w3.org/html/logo/badge/html5-badge-h-css3-semantics.png" width="165" height="64" alt="HTML5 Powered with CSS3 / Styling, and Semantics" title="HTML5 Powered with CSS3 / Styling, and Semantics"></a>
+				</li>
+			<li>
+				<!-- Creative Commons !-->
+				<a class="nolink" rel="license" href="http://creativecommons.org/licenses/by-nc-nd/3.0/deed.en_US"><img alt="Creative Commons License" style="border-width:0" src="http://i.creativecommons.org/l/by-nc-nd/3.0/80x15.png" /></a>
+			</li>
+			</ul>
+		</div>
 </footer>
 <?php wp_footer(); ?>
 </body>
--- wp-content/themes/valecappneus/page.php
+++ wp-content/themes/valecappneus/page.php
@@ -7,8 +7,24 @@
 
 get_header(); ?>
 
-<nav class="container_12">
+<nav class="grid_12">
 	<ol class="breadcrumb"><?php the_breadcrumb(); ?></ol>
 </nav>
+
+<?php if ( have_posts() ) while ( have_posts() ) : the_post(); ?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+				<?php if ( is_front_page() ) { ?>
+					<h2 class="entry-title"><?php the_title(); ?></h2>
+				<?php } else { ?>	
+					<h1 class="entry-title"><?php the_title(); ?></h1>
+				<?php } ?>
+					<div class="entry-content">
+						<?php the_content(); ?>
+						<?php wp_link_pages( array( 'before' => '' . __( 'Pages:', 'boilerplate' ), 'after' => '' ) ); ?>
+						<?php edit_post_link( __( 'Edit', 'boilerplate' ), '', '' ); ?>
+					</div><!-- .entry-content -->
+				</article><!-- #post-## -->
+				<?php comments_template( '', true ); ?>
+<?php endwhile; ?>
 
 <?php get_footer(); ?>
