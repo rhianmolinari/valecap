8b7e3db8c1342a426fdb5dffc2124533f6140cba
alexandre <alexandre@jacare>
2013-02-14 17:59:19 -0200

    adicionando novo box

--- wp-content/themes/valecappneus/functions.php
+++ wp-content/themes/valecappneus/functions.php
@@ -373,5 +373,126 @@ function save_product_meta($post_id) {
 
 add_action('save_post', 'save_product_meta');
 
+function add_xyz_meta_box() {
+    add_meta_box(
+            'xyz_meta_box', // $id  
+            'P&aacute;gina inicial', // $title   
+            'show_xyz_meta_box', // $callback  
+            'produto', // $page  
+            'side', // $context  
+            'high'); // $priority  
+}
+
+add_action('add_meta_boxes', 'add_xyz_meta_box');
+
+$prefix_xyz = 'xyz_';
+$custom_xyz_fields = array(
+    array(
+        'label' => 'Produto em destaque',
+        'desc' => 'Produto em destaque para a p&aacute;gina inicial',
+        'id' => $prefix_xyz . 'dest_post_id',
+        'type' => 'post_list',
+        'post_type' => array('produto')
+    ),
+    array(
+        'label' => 'Mais vendidos',
+        'desc' => '1&#186; lugar',
+        'id' => $prefix_xyz . 'first_post_id',
+        'type' => 'post_list',
+        'post_type' => array('produto')
+    ),
+    array(
+        'label' => '',
+        'desc' => '2&#186; lugar',
+        'id' => $prefix_xyz . 'second_post_id',
+        'type' => 'post_list',
+        'post_type' => array('produto')
+    ),
+    array(
+        'label' => '',
+        'desc' => '3&#186; lugar',
+        'id' => $prefix . 'third_post_id',
+        'type' => 'post_list',
+        'post_type' => array('produto')
+    )
+);
+
+function show_xyz_meta_box() {
+    global $custom_xyz_fields, $post;
+// Use nonce for verification  
+    echo '<input type="hidden" name="custom_xyz_box_nonce" value="' . wp_create_nonce(basename(__FILE__)) . '" />';
+
+    // Begin the field table and loop  
+    echo '<table class="form-table">';
+    foreach ($custom_xyz_fields as $field) {
+        // get value of this field if it exists for this post  
+        $meta = get_post_meta($post->ID, $field['id'], true);
+        // begin a table row with  
+        echo '<tr> 
+                <th><label for="' . $field['id'] . '">' . $field['label'] . '</label></th> 
+                <td>';
+
+        switch ($field['type']) {
+            // post_list  
+            case 'post_list':
+                $items = get_posts(array(
+                    'post_type' => $field['post_type'],
+                    'posts_per_page' => -1
+                        ));
+                $atual = get_posts(array(
+                    'post_type' => $field['post_type'],
+                    'posts_per_page' => 1,
+                    'meta_query' => array(
+                        array(
+                            'key' => $field['id']
+                        )
+                    )
+                        ));
+                echo '<select name="' . $field['id'] . '" id="' . $field['id'] . '"> 
+            <option value="">Selecione um produto</option>'; // Select One  
+                foreach ($items as $item) {
+                    echo '<option value="' . $item->ID . '"', $atual[0]->ID == $item->ID ? ' selected="selected"' : '', '>' . $item->post_title . '</option>';
+                } // end foreach  
+                echo '</select><br /><span class="description">' . $field['desc'] . '</span>';
+                break;
+        }
+        echo '</td></tr>';
+    } // end foreach  
+    echo '</table>'; // end table  
+}
+
+function save_xyz_meta($post_id) {
+    global $custom_xyz_fields;
+
+    // verify nonce
+    if (isset($_POST['custom_xyz_box_nonce'])) {
+        if (!wp_verify_nonce($_POST['custom_xyz_box_nonce'], basename(__FILE__)))
+            return $post_id;
+    }
+    // check autosave  
+    if (defined('DOING_AUTOSAVE') && DOING_AUTOSAVE)
+        return $post_id;
+    // check permissions  
+    if ('page' == $_POST['post_type']) {
+        if (!current_user_can('edit_page', $post_id))
+            return $post_id;
+    } elseif (!current_user_can('edit_post', $post_id)) {
+        return $post_id;
+    }
+
+    // loop through fields and save the data  
+    foreach ($custom_xyz_fields as $field) {
+        $old = get_post_meta($post_id, $field['id'], true);
+        $new = $_POST[$field['id']];
+        if ($new && $new != $old) {
+            update_post_meta($post_id, $field['id'], $new);
+        } elseif ('' == $new && $old) {
+            delete_post_meta($post_id, $field['id'], $old);
+        }
+    } // end foreach  
+}
+
+add_action('save_post', 'save_xyz_meta');
+
 add_post_type_support('produto', array('thumbnail', 'tags'));
 ?>
