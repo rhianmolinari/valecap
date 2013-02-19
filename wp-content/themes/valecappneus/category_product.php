c8e5ba21b640184cfabd16229e26553808907267
alexandre <alexandre@jacare>
2013-02-19 11:17:14 -0300

    adicionando taxonomias dinamicamente de acordo com a taxonomia do tipo produtos.

--- wp-content/themes/valecappneus/category_product.php
+++ wp-content/themes/valecappneus/category_product.php
@@ -14,49 +14,17 @@ get_header(); ?>
 
 <article class="grid_12">
 	<h1><?php the_title(); ?></h1>
+        <?php $categories = get_categories(array('type' => 'produto', 'orderby' => 'id','order' => 'ASC', 'taxonomy' => 'tipo_produto', 'hide_empty' => 0));$classez = array(' alpha','','',' omega');$cont=0; ?>
 		<ul class="type-product">
-			<li class="box grid_3 alpha">
-				<a href="#">
-					<h3>Passeio / Utilitários</h3>
-					<img src="http://placehold.it/190x190">
+                        <?php foreach($categories as $category): ?>
+			<li class="box grid_3 productbox<?php echo $classez[$cont++];$cont = ($cont%4 == 0)?0:$cont; ?>">
+                            <a href="<?php echo get_category_link($category->term_id); ?>">
+					<h3><?php echo $category->cat_name; ?></h3>
+                                        <img src="<?php echo (function_exists('z_taxonomy_image_url'))?z_taxonomy_image_url($category->term_id):"http://placehold.it/190x190"; ?>">
 					<span class="saibamais">+ saiba mais</span>
 				</a>
 			</li>
-			<li class="box grid_3">
-				<a href="#">
-					<h3>Off road</h3>
-					<img src="http://placehold.it/190x190">
-					<span class="saibamais">+ saiba mais</span>
-				</a>
-			</li>
-			<li class="box grid_3">
-				<a href="#">
-					<h3>Carga</h3>
-					<img src="http://placehold.it/190x190">
-					<span class="saibamais">+ saiba mais</span>
-				</a>
-			</li>
-			<li class="box grid_3 omega">
-				<a href="#">
-					<h3>Agrícola</h3>
-					<img src="http://placehold.it/190x190">
-					<span class="saibamais">+ saiba mais</span>
-				</a>
-			</li>
-			<li class="box grid_3 alpha">
-				<a href="#">
-					<h3>Terraplanagem</h3>
-					<img src="http://placehold.it/190x190">
-					<span class="saibamais">+ saiba mais</span>
-				</a>
-			</li>
-			<li class="box grid_3">
-				<a href="#">
-					<h3>Industriais</h3>
-					<img src="http://placehold.it/190x190">
-					<span class="saibamais">+ saiba mais</span>
-				</a>
-			</li>
+                        <?php endforeach; ?>
 		</ul>
 </article>
 
