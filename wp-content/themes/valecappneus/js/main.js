78bd9384b2068c81fa477e3bf678a0d06c56bbe8
Alexandre <Alexandre@itaguare>
2013-02-20 02:11:51 -0300

    Acertando ajax listagem produtos

--- wp-content/themes/valecappneus/js/main.js
+++ wp-content/themes/valecappneus/js/main.js
@@ -64,20 +64,53 @@ $(document).ready(function(){
 
 // Abas listagem produtos
 $(document).ready(function(){
-	$('.abas ul li a').click(function(){
+    
+    var slugi = location.hash;
+    slugi = slugi.substring(slugi.lastIndexOf('/')+1);
+    $('#categoria-produtos ul li').removeClass('selected');
+    $('#'+slugi).parent().addClass('selected');
+    $.ajax({
+        type: "GET",
+        url: "/wp-admin/admin-ajax.php",
+        data: 'action=tipo_produto&slug='+slugi,
+        dataType: 'json',
+        success: getProdutos
+    });
+    
+	$('#categoria-produtos ul li a').click(function(){
             var slug = $(this).attr('href');
             slug = slug.substring(slug.lastIndexOf('/')+1);
+            $('#categoria-produtos ul li').removeClass('selected');
+            $(this).parent().addClass('selected');
             $.ajax({
                 type: "GET",
                 url: "/wp-admin/admin-ajax.php",
                 data: 'action=tipo_produto&slug='+slug,
                 dataType: 'json',
-                success: function(data) {
-                    alert(data);
-                }
+                success: getProdutos
             });
 	});
 });
+
+function getProdutos(data) {
+    if(data != null) {
+        var classez = [' alpha','',' omega'];
+        var cont = 0;
+        $('#products').html('');
+        for(var i=0;i<data.length;i++) {
+            var li = document.createElement('li');li.setAttribute('class', 'box grid_3 productbox'+classez[cont]);
+            var a = document.createElement('a');a.setAttribute('href', data[i][0].link);
+            var h3 = document.createElement('h3');h3.appendChild(document.createTextNode(data[i][0].title));
+            var img = document.createElement('img');img.setAttribute('src', (data[i][0].img == null)?'':data[i][0].img);
+            var span = document.createElement('span');span.setAttribute('class', 'saibamais');span.appendChild(document.createTextNode('+ saiba mais'));
+            a.appendChild(h3);a.appendChild(img);a.appendChild(span);
+            li.appendChild(a);
+            if(cont++ == 2)cont=0;
+            $('#products').append(li);
+        }
+    }else
+        alert('No momento não há produtos nesta categoria!');
+}
 
 // Dropkick
 $(document).ready(function(){
